ee865c6410a5a0d60cfc0e7c80db3d4d1323b87e
andy.hupel <hupel.andy@gmail.com>
2014-06-30 20:12:01 +0200

    Bug in Menubar is fixed. - Renaming the exit handler.

--- SportsBrain/source/eclipse.sportsBrain.e4.cards/src/eclipse/sportsBrain/e4/cards/handlers/ExitHandler.java
+++ SportsBrain/source/eclipse.sportsBrain.e4.cards/src/eclipse/sportsBrain/e4/cards/handlers/ExitHandler.java
@@ -3,7 +3,7 @@ package eclipse.sportsBrain.e4.cards.handlers;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.workbench.IWorkbench;
 
-public class ExitHandlerImpl {
+public class ExitHandler {
 	@Execute
 	public void execute(IWorkbench workbench) {
 			workbench.close();
